941d57ea37a28c89c44ddb8fe29d853aa086d02f
DanielPastorRueda <dankartelecom@gmail.com>
2015-11-09 11:18:53 +0100

    prueba desde netbeans

--- Tema6/ejercicios/07.php
+++ Tema6/ejercicios/07.php
@@ -1,11 +1,11 @@
 <?php
 
-    /*
-      Daniel Pastor Rueda.
+/* Daniel Pastor Rueda. Ejercicio 07
+
 Escribe un programa que guarde en una cookie el color de
 fondo (propiedad <b>background-color</b>) de una página. Esta página
 debe tener únicamente algo de texto y un formulario para cambiar el
 color.
 
-     */
+*/
 ?>
